4368419f2ba040635c824ed2a5bea28ae6e890f5
aaravhajare <hajareaarav66@gmail.com>
2025-04-27 09:38:01 +0530

    Employee Inheritance

---
+++ Employee Inheritance.py
@@ -0,0 +1,23 @@
+class person () :
+
+    def __init__(self,name,idnumber):
+
+        self.name = name
+        self.idnumber = idnumber
+
+    def display(self) :
+
+        print(self.name)
+        print(self.idnumber)
+
+class employee(person):
+
+    def __init__(self, name, idnumber , post , salary):
+        self.salary = salary
+        self.post = post
+
+        person.__init__(self , name , idnumber)
+
+a = employee("Aahan" , 9999999999 , "fresher" , 0.9)
+
+a.display()
